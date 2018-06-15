e2ef45f94558b6d0a9bf7cd2ec811eb7bd8ce116
Priscila Andreani <priscilaandreani@gmail.com>
2018-06-15 08:42:49 -0300

    LISTAS JAVASCRÍPT

---
+++ index.js
@@ -0,0 +1,14 @@
+/* 
+let lista [x1,x2,x3,x4]
+let lista []  //lista vazia
+lista.push(false) //adiciona um ícone a lista, na ultima posição
+lista.unshift(true) //adiciona um ícone ao início da lista
+lista[3] = undefined //para esvaziar algum ítem da lista
+
+Para adicionar um objeto com um novo valor pode-se usar ponto
+let cliente = {nome: "Renan", login: "gerbilo", "senha:aviao11"}
+cliente.vip = true
+cliente = {nome: "Renan", login: "gerbilo", "senha:aviao11", vip: true}
+delete cliente.vip
+{nome: "Renan", login: "gerbilo", "senha:aviao11"}
+*/
